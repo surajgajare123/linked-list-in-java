23263a93ed904b124c8b140da1d9172c4efa2bcb
Suraj <108755893+surajgajare123@users.noreply.github.com>
2024-12-13 22:44:40 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		LinkedList<Integer> l1 = new LinkedList<>();
+		l1.add(23);
+		l1.add(34);
+		l1.add(44);
+		l1.add(55);
+		System.out.println(l1);
+	}
+}
